0b1caaa8d2fdcbc0d27230d3e92f179fb537d67d
nchhillar2004 <nchhillar124507@gmail.com>
2024-02-04 21:19:38 +0530

    final commit

---
+++ app/privacy/page.tsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function Privacy() {
+  return (
+    <div>Privacy</div>
+  )
+}
--- config/site.ts
+++ config/site.ts
@@ -2,8 +2,8 @@ const SiteConfig = {
     title: "Baba haridas financial services",
     shortTitle: "BHFS",
     description: "Helping you with money matters through a user-friendly website - simple, effective, and just for you",
-    url: "https://app.nishantchhillar.tech",
-    ogImage: "https://app.nishantchhillar.tech/og.jpg",
+    url: "https://nishantchhillar.tech",
+    ogImage: "https://nishantchhillar.tech/og.jpg",
     links: {
         github: "https://github.com/nchhillar2004/",
         discord: "https://discord.com/",
