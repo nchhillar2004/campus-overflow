670ca0fe381cae74587c1b4dc611c3bbac90cad5
nchhillar2004 <nchhillar124507@gmail.com>
2024-02-05 21:41:25 +0530

    final commit

--- config/site.ts
+++ config/site.ts
@@ -2,7 +2,7 @@ const SiteConfig = {
     title: "Baba haridas financial services",
     shortTitle: "BHFS",
     description: "Helping you with money matters through a user-friendly website - simple, effective, and just for you",
-    url:  "http://localhost:3000" || "https://nishantchhillar.tech",
+    url:  "https://nishantchhillar.tech",
     ogImage: "https://nishantchhillar.tech/og.jpg",
     links: {
         github: "https://github.com/nchhillar2004/",
