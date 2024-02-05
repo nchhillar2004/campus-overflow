65c0772142b3e766f6c7ce3a09c6b077d66e7b9c
nchhillar2004 <nchhillar124507@gmail.com>
2024-02-05 21:59:22 +0530

    final commit

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { TypographyH1 } from "@/components/typography";
 import { ModeToggle } from "@/components/theme-toggle-mode";
+import Link from "next/link";
 
 export default function HomePage() {
     return (
@@ -8,6 +9,12 @@ export default function HomePage() {
             <div className="container">
                 <ModeToggle/>
                 <TypographyH1 title="Welcome to BH Financial Services" />
+                <br />
+                <Link href='/posts'>Posts</Link>
+                <br />
+                <Link href='/u'>Users</Link>
+                <br />
+                <Link href='/u/profile'>Your Profile</Link>
             </div>
         </div>
     );
