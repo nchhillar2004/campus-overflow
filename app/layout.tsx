88dcba20f40ef8960833966c9615ba7ee7197af8
Nishant Chhillar <nchhillar2004@gmail.com>
2024-04-16 12:52:09 +0530

    Site config

--- app/layout.tsx
+++ app/layout.tsx
@@ -9,8 +9,6 @@ import { getServerSession } from "next-auth";
 import SiteConfig from "@/config/site";
 import { Analytics } from "@vercel/analytics/react";
 import { SpeedInsights } from "@vercel/speed-insights/next";
-import { Suspense } from "react";
-import { Loading } from "@/components/loading";
 
 export const metadata: Metadata = {
     title: {
--- config/site.ts
+++ config/site.ts
@@ -1,12 +1,12 @@
 const isProduction = process.env.NODE_ENV === 'production';
-const productionUrl = process.env.NEXTAUTH_URL;
+const productionUrl = 'https://nextjs-finance-app-lake.vercel.app';
 
 const SiteConfig = {
     title: "Baba haridas financial services",
     shortTitle: "BHFS",
     description: "Helping you with money matters through a user-friendly website - simple, effective, and just for you",
     url: isProduction ? productionUrl : 'http://localhost:3000',
-    ogImage: `${productionUrl}og.jpg`,
+    ogImage: `${productionUrl}/og.jpg`,
     links: {
         github: "https://github.com/nchhillar2004/",
         discord: "https://discord.com/",
